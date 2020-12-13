0f820600b890820cf91353d7b207fa7159356224
katungi <dankatdennis@gmail.com>
2020-12-13 12:10:22 +0300

    added custom hooks 🌭

--- pages/experiences.js
+++ pages/experiences.js
@@ -1,21 +1,25 @@
 import {useEffect} from 'react';
+import { useQuery, useMutation, useQueryCache, QueryCache, ReactQueryCacheProvider } from 'react-query'
 import Cards from '../components/experienceCards/cards'
-import axios from 'axios';
 import Categories from '../components/categories'
 import Footer from '../components/footer'
 import { Divider } from '@material-ui/core';
+import { useAxiosGet } from '../Hooks/HttpRequests';
+
+const queryCache = new QueryCache();  
 
 export default function experiences() {
-  
-    // useEffect(()=>{
-        
-    // },[])
+
+    const url = 'http://localhost:3001/api/v1/meetups'
+    const experiences = useAxiosGet(url);
+    console.log(experiences);
+
    return(
     <div>
         <h1 className="text-4xl font-extrabold text-pink-600 uppercase">Experiences</h1>
         <br/>
         <Divider/>
-        <Categories/>
+        <Cards></Cards>
         <Footer/>
     </div>
    );
