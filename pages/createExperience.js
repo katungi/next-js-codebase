290544618b9c31b13cc1f0f9c392dc7111de8ffe
katungi <dankatdennis@gmail.com>
2021-01-09 12:26:19 +0300

    failed select value

--- pages/createExperience.js
+++ pages/createExperience.js
@@ -1,15 +1,12 @@
-import Link from 'next/link'
-import {
-  Form,
-  Select,
-  Button,
-  Upload,
-  Input
-} from "antd";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { Form, Select, Button, Upload, Input } from "antd";
 import { InboxOutlined } from "@ant-design/icons";
-import Axios from 'axios';
 
+const { Option } = Select;
 export default function createExperiences() {
+  const [categories, setCategories] = useState([]);
+
   const { Option } = Select;
   const formItemLayout = {
     labelCol: { span: 6 },
@@ -22,13 +19,30 @@ export default function createExperiences() {
   const onFinish = (values) => {
     console.log("Received values of form: ", values);
   };
+
+  useEffect(() => {
+    let initialCategories = [];
+
+    fetch("http://localhost:8000/api/categories")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        initialCategories = data.map((category) => {
+          return category;
+        });
+        console.log(initialCategories);
+        setCategories(initialCategories);
+      });
+  }, []);
+
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
+
   return (
     <>
-      <Form
-        name="validate_other"
-        {...formItemLayout}
-        onFinish={onFinish}
-      >
+      <Form name="validate_other" {...formItemLayout} onFinish={onFinish}>
         <Form.Item
           name="Experience"
           label="Experience"
@@ -51,32 +65,37 @@ export default function createExperiences() {
             },
           ]}
         >
-          <Select placeholder="Please select The Category for Experience">
-            <Option value="china">China</Option>
-            <Option value="usa">U.S.A</Option>
+          <Select
+            placeholder="Please select The Category for Experience"
+            onChange={handleChange}
+          >
+            {categories.map((category, index) => {
+              console.log(`${category.name}, ${index}`);
+              <Option key={category._id}>{category.name}</Option>;
+            })}
           </Select>
         </Form.Item>
-          <Form.Item
-            name="dragger"
-            valuePropName="fileList"
-            getValueFromEvent={normFile}
-            noStyle
-          >
-            <Upload.Dragger name="files" action={normFile}>
-              <p className="ant-upload-drag-icon">
-                <InboxOutlined />
-              </p>
-              <p className="ant-upload-text">
-                Click or drag file to this area to upload
-              </p>
-              <p className="ant-upload-hint">
-                Support for a single or bulk upload.
-              </p>
-            </Upload.Dragger>
+        <Form.Item
+          name="dragger"
+          valuePropName="fileList"
+          getValueFromEvent={normFile}
+          noStyle
+        >
+          <Upload.Dragger name="files" action={normFile}>
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">
+              Click or drag file to this area to upload
+            </p>
+            <p className="ant-upload-hint">
+              Support for a single or bulk upload.
+            </p>
+          </Upload.Dragger>
         </Form.Item>
         <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
-          <Button type="primary" htmlType="submit" >
-            <Link href="/"><a>Submit</a></Link>
+          <Button type="primary" htmlType="submit">
+            Submit
           </Button>
         </Form.Item>
       </Form>
