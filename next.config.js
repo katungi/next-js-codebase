6dfa82c3f263ffb83030e0baf612be2a14b13ff3
katungi <dankatdennis@gmail.com>
2021-01-10 23:09:45 +0300

    migrated next.config to es6

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-module.exports  = {
-    env: {
-        APP_NAME: 'HostGuest',
-        API_DEVELOPMENT: "http://localhost:8000/",
-        API_PRODUCTION: "https://hostguest-backend.herokuapp.com/",
-        PRODUCTION: true,
-        GOOGLE_CLIENT_ID : "722419178757-le6fn8kpvjo7dch9nl6vg4jlo01mm150.apps.googleusercontent.com"
-    }
+export const env = {
+    APP_NAME: 'HostGuest',
+    API_DEVELOPMENT: "http://localhost:8000/",
+    API_PRODUCTION: "https://hostguest-backend.herokuapp.com/",
+    PRODUCTION: true,
+    GOOGLE_CLIENT_ID: "722419178757-le6fn8kpvjo7dch9nl6vg4jlo01mm150.apps.googleusercontent.com"
 };
