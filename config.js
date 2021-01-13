ab9db96c3724139d7d90c7ac7370a52a9c4f8b51
katungi <dankatdennis@gmail.com>
2021-01-13 12:05:10 +0300

    cleaned up codebase

--- config.js
+++
@@ -1,8 +0,0 @@
-// import getConfig from "next/config";
-// const { env } = getConfig();
-
-// export const API = env.production
-//   ? `${env.API_PRODUCTION}`
-//   : `${env.API_DEVELOPMENT}`;
-// export const APP_NAME = env.APP_NAME;
-// export const GOOGLE_CLIENT_ID = env.GOOGLE_CLIENT_ID;
