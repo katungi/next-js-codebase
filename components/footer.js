61ea8e14a8dc7d66904121f319054baf93812e41
katungi <dankatdennis@gmail.com>
2021-01-26 11:33:37 +0300

    changed redirect URI

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,11 @@
 import style from "../styles/footer.module.css";
 export default function Footer() {
   return (
-    <div className={style.footer}>
-      <p>Made With Love by the HostGuest Team 2021</p>
-      <p>Privacy · Terms · Sitemap · Company Details</p>
-    </div>
+    <footer>
+      <div className={style.footer}>
+        <p>Made With Love by the HostGuest Team 2021</p>
+        <p>Privacy · Terms · Sitemap · Company Details</p>
+      </div>
+    </footer>
   );
 }
