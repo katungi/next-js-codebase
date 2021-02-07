0fee5433c8c57ee0e5c543b17c8a58c378161b91
katungi <dankatdennis@gmail.com>
2021-02-07 11:40:14 +0300

    changed to react icons from fafafa

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,14 @@
-import Link from 'next/link';
+import { GoPencil, GoPerson } from 'react-icons/go';
+import {
+  AiFillDollarCircle,
+  AiFillDashboard,
+  AiFillSchedule,
+  AiFillBulb,
+  AiFillSafetyCertificate,
+  AiOutlineHourglass,
+  AiOutlineNotification,
+  AiOutlineHighlight,
+} from 'react-icons/ai';
 import Footer from '../components/footer';
 
 export default function About() {
@@ -54,7 +64,7 @@ export default function About() {
                   <div className='relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg'>
                     <div className='px-4 py-5 flex-auto'>
                       <div className='text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400'>
-                        <i className='fas fa-award'></i>
+                        <GoPerson />
                       </div>
                       <h6 className='text-xl font-semibold'>
                         Register and get approved
@@ -70,7 +80,7 @@ export default function About() {
                   <div className='relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg'>
                     <div className='px-4 py-5 flex-auto'>
                       <div className='text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-400'>
-                        <i className='fas fa-retweet'></i>
+                        <GoPencil />
                       </div>
                       <h6 className='text-xl font-semibold'>
                         Create and Impress
@@ -86,7 +96,7 @@ export default function About() {
                   <div className='relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg'>
                     <div className='px-4 py-5 flex-auto'>
                       <div className='text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400'>
-                        <i className='fas fa-fingerprint'></i>
+                        <AiFillDollarCircle />
                       </div>
                       <h6 className='text-xl font-semibold'>Make Money</h6>
                       <p className='mt-2 mb-4 text-gray-600'>
@@ -100,7 +110,7 @@ export default function About() {
               <div className='flex flex-wrap items-center mt-32'>
                 <div className='w-full md:w-5/12 px-4 mr-auto ml-auto'>
                   <div className='text-gray-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-gray-100'>
-                    <i className='fas fa-user-friends text-xl'></i>
+                    <AiFillDashboard />
                   </div>
                   <h3 className='text-3xl mb-2 font-semibold leading-normal'>
                     Our Mission
@@ -178,7 +188,7 @@ export default function About() {
                 <div className='w-full md:w-5/12 ml-auto mr-auto px-4'>
                   <div className='md:pr-12'>
                     <div className='text-pink-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-pink-300'>
-                      <i className='fas fa-rocket text-xl'></i>
+                      <AiOutlineHighlight />
                     </div>
                     <h3 className='text-3xl font-semibold'>
                       A few necessities before you Host
@@ -192,7 +202,7 @@ export default function About() {
                         <div className='flex items-center'>
                           <div>
                             <span className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3'>
-                              <i className='fas fa-fingerprint'></i>
+                              <AiOutlineHourglass />
                             </span>
                           </div>
                           <div>
@@ -206,7 +216,7 @@ export default function About() {
                         <div className='flex items-center'>
                           <div>
                             <span className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3'>
-                              <i className='fab fa-html5'></i>
+                              <AiFillSafetyCertificate />
                             </span>
                           </div>
                           <div>
@@ -220,7 +230,7 @@ export default function About() {
                         <div className='flex items-center'>
                           <div>
                             <span className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3'>
-                              <i className='far fa-paper-plane'></i>
+                              <AiOutlineNotification />
                             </span>
                           </div>
                           <div>
@@ -268,7 +278,7 @@ export default function About() {
               <div className='flex flex-wrap mt-12 justify-center'>
                 <div className='w-full lg:w-3/12 px-4 text-center'>
                   <div className='text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center'>
-                    <i className='fas fa-medal text-xl'></i>
+                    <AiFillSafetyCertificate />
                   </div>
                   <h6 className='text-xl mt-5 font-semibold text-white'>
                     Register to be a Host
@@ -280,7 +290,7 @@ export default function About() {
                 </div>
                 <div className='w-full lg:w-3/12 px-4 text-center'>
                   <div className='text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center'>
-                    <i className='fas fa-poll text-xl'></i>
+                    <AiFillBulb />
                   </div>
                   <h5 className='text-xl mt-5 font-semibold text-white'>
                     Think of an Idea
@@ -292,7 +302,7 @@ export default function About() {
                 </div>
                 <div className='w-full lg:w-3/12 px-4 text-center'>
                   <div className='text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center'>
-                    <i className='fas fa-lightbulb text-xl'></i>
+                    <AiFillSchedule />
                   </div>
                   <h5 className='text-xl mt-5 font-semibold text-white'>
                     Host the Experience
@@ -374,7 +384,7 @@ export default function About() {
           </section>
         </main>
       </div>
-      <Footer/>
+      <Footer />
       <style jsx>{`
         .mainHeight {
           min-height: 75vh;
